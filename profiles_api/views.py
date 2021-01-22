d1338891ca881d79a5f755d8f03860c755f57d95
Christophe Lucchini <christophelucchini@sfr.fr>
2021-01-22 21:37:51 +0100

    Added permission vieswset feed

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,12 +2,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
-from rest_framework.authentication import TokenAuthentication
-from rest_framework import filters
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -108,3 +111,20 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserProfileFeedSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated
+    )
+    
+    def perform_create(self, serializer):
+        serializer.save(user_profile=self.request.user)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
